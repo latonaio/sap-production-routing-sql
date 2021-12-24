87d16de7c11598fa42bbf4d6e9134ee0f1815c55
Yurino Oga <yurino.o@latona.io>
2021-12-24 18:30:07 +0900

    release

--- sap-production-routing-sql.sql
+++ sap-production-routing-sql.sql
@@ -1,9 +1,10 @@
 CREATE TABLE `sap-production-routing`
 (
-    `Product`                varchar(40) DEFAULT NULL,
-    `Plant`                  varchar(4) DEFAULT NULL,
-    `ProductionRoutingGroup` varchar(8) DEFAULT NULL,
-    `ProductionRouting`      varchar(2) DEFAULT NULL,
+    `Product`                varchar(40) NOT NULL,
+    `Plant`                  varchar(4) NOT NULL,
+    `ProductionRoutingGroup` varchar(8) NOT NULL,
+    `ProductionRouting`      varchar(2) NOT NULL,
+    `ProductionRoutingOpIntID` varchar(8) NOT NULL,
     `ProductionRoutingGroup_desc` varchar(40) DEFAULT NULL,
     `BillOfOperationsStatus` varchar(3) DEFAULT NULL,
     `MinimumLotSizeQuantity` varchar(13) DEFAULT NULL,
@@ -15,7 +16,6 @@ CREATE TABLE `sap-production-routing`
     `BillOfMaterial`         varchar(8) DEFAULT NULL,
     `BillOfMaterialItemNodeNumber` varchar(8) DEFAULT NULL,
     `MatlCompIsMarkedForBackflush` tinyint(1) DEFAULT NULL,
-    `ProductionRoutingOpIntID` varchar(8) DEFAULT NULL,
     `WorkCenterInternalID`   varchar(8) DEFAULT NULL,
     `Operation`              varchar(40) DEFAULT NULL,
     `OperationText`          varchar(40) DEFAULT NULL,
@@ -26,6 +26,7 @@ CREATE TABLE `sap-production-routing`
     `PlannedDeliveryDuration` varchar(3) DEFAULT NULL,
     `NumberOfOperationPriceUnits` varchar(5) DEFAULT NULL,
     `OpExternalProcessingPrice` varchar(11) DEFAULT NULL,
+    
     PRIMARY KEY (`Product`, `Plant`, `ProductionRoutingGroup`, `ProductionRouting`, `ProductionRoutingOpIntID`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
